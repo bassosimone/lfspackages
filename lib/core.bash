14bd7794b0bc8bda9ee7bfc5a623afe2e97629a2
Simone Basso <bassosimone@gmail.com>
2023-12-28 00:39:47 +0100

    feat: modularize install scripts
    We need to build individual packages, link them, or just
    install a package and all its dependencies.

--- lib/core.bash
+++ lib/core.bash
@@ -85,3 +85,98 @@ pkg_lib_maybe_copy_etc_all() {
         pkg_lib_run sudo cp -p $file $dest/$corename
     done
 }
+
+# pkg_cli_build DIR [...] builds the package identified by DIR. Any
+# argument passed after DIR is executed after we have built the package.
+pkg_cli_build() {
+    local dir_name=$1
+    shift
+
+    if [[ ! -f $dir_name/package.bash ]]; then
+        echo "FATAL: $dir_name/package.bash: no such file or directory" 1>&2
+        exit 1
+    fi
+
+    (
+        . $dir_name/package.bash
+
+        __destdir=$(pkg_print_destdir)
+        if [[ -d $__destdir ]]; then
+            pkg_lib_info "package $dir_name already installed at $__destdir"
+            return
+        fi
+
+        (
+            # ensure packages find the /opt alternative root
+            pkg_lib_run export PKG_CONFIG_PATH="/opt/lib/pkgconfig:/usr/lib/pkgconfig"
+            pkg_lib_run export CFLAGS="-I/opt/include ${CFLAGS:-}"
+            pkg_lib_run export LDFLAGS="-L/opt/lib ${LDFLAGS:-}"
+
+            WORKDIR=$(pkg_lib_run mktemp -d)
+
+            pkg_lib_run cd $WORKDIR
+
+            pkg_build
+
+            pkg_lib_run rm -rf $WORKDIR
+        )
+
+        # execute the remaining arguments
+        (
+            if [[ $# -gt 0 ]]; then
+                "$@"
+            fi
+        )
+    )
+}
+
+# pkg_cli_link DIR links the package identified by DIR.
+pkg_cli_link() {
+    local dir_name=$1
+
+    if [[ ! -f $dir_name/package.bash ]]; then
+        echo "FATAL: $dir_name/package.bash: no such file or directory" 1>&2
+        exit 1
+    fi
+
+    (
+        . $dir_name/package.bash
+
+        __destdir=$(pkg_print_destdir)
+        if [[ ! -d $__destdir ]]; then
+            pkg_lib_warn "package $dir_name not installed at $__destdir"
+            return
+        fi
+
+        pkg_link
+    )
+}
+
+# pkg_cli_install DIR combines pkg_cli_build and pkg_cli_link
+pkg_cli_install() {
+    local dir_name=$1
+    pkg_cli_build $dir_name pkg_cli_link $dir_name # pipelining execution
+}
+
+# pkg_cli_install_recursive DIR installs the package defined by the
+# given DIR and all its dependencies recursively.
+pkg_cli_install_recursive() {
+    local dir_name=$1
+
+    if [[ ! -f $dir_name/package.bash ]]; then
+        echo "FATAL: $dir_name/package.bash: no such file or directory" 1>&2
+        exit 1
+    fi
+
+    (
+        . $dir_name/package.bash
+
+        for dependency in $(pkg_print_deps); do
+            (
+                pkg_cli_install_recursive $dependency
+            )
+        done
+    )
+
+    pkg_cli_install $dir_name
+}
