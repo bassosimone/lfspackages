8028306dcd877e8714c95c643f00719eec84e447
Simone Basso <bassosimone@gmail.com>
2023-12-28 18:02:44 +0100

    chore(ooniguruma): make declarative

--- ports/o/oniguruma/oniguruma-6.9.9/package.bash
+++ ports/o/oniguruma/oniguruma-6.9.9/package.bash
@@ -1,27 +1,12 @@
-pkg_print_deps() {
-    echo ""
-}
-
-pkg_print_destdir() {
-    echo "/opt/package/oniguruma-6.9.9"
-}
-
-pkg_build() {
-    pkg_lib_download https://github.com/kkos/oniguruma/releases/download/v6.9.9/onig-6.9.9.tar.gz
-    pkg_lib_verify onig-6.9.9.tar.gz 60162bd3b9fc6f4886d4c7a07925ffd374167732f55dce8c491bfd9cd818a6cf
-    pkg_lib_extract onig-6.9.9.tar.gz
-
-    pkg_lib_run cd onig-6.9.9
-
-    pkg_lib_run ./configure --prefix=/opt/package/oniguruma-6.9.9
-
-    pkg_lib_run make -j$(nproc)
-
-    pkg_lib_run sudo make install
-}
-
-pkg_link() {
-    for dirname in bin include lib; do
-        pkg_lib_symlink_all /opt/package/oniguruma-6.9.9/$dirname /opt/$dirname
-    done
-}
+__pkg_sha256=60162bd3b9fc6f4886d4c7a07925ffd374167732f55dce8c491bfd9cd818a6cf
+__pkg_name=oniguruma
+__pkg_version=6.9.9
+__pkg_distro_name=${__pkg_name}-${__pkg_version}
+__pkg_src_name=onig-${__pkg_version}
+__pkg_tarball_name=onig-${__pkg_version}.tar.gz
+__pkg_tarball_url=https://github.com/kkos/oniguruma/releases/download/v${__pkg_version}/${__pkg_tarball_name}
+__pkg_configure_extra_args=()
+__pkg_build_type=autotools
+__pkg_deps=()
+__pkg_link_dirs=(bin include lib)
+__pkg_maybe_copy_persistent_config=()
