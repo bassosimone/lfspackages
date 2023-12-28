531676db1f5c072737622b1fc99b50f2f464eb01
Simone Basso <bassosimone@gmail.com>
2023-12-28 01:33:15 +0100

    feat: package gnupg

---
+++ ports/g/gnupg-2.4.3/package.bash
@@ -0,0 +1,34 @@
+pkg_print_deps() {
+    deps=(
+        ./ports/l/libassuan-2.5.6
+        ./ports/l/libgcrypt-1.10.2
+        ./ports/l/libksba-1.6.4
+        ./ports/n/npth-1.6
+    )
+    echo "${deps[@]}"
+}
+
+pkg_print_destdir() {
+    echo "/opt/package/gnupg-2.4.3"
+}
+
+pkg_build() {
+    pkg_lib_download https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.4.3.tar.bz2
+    pkg_lib_verify gnupg-2.4.3.tar.bz2 a271ae6d732f6f4d80c258ad9ee88dd9c94c8fdc33c3e45328c4d7c126bd219d
+    pkg_lib_extract gnupg-2.4.3.tar.bz2
+
+    pkg_lib_run cd gnupg-2.4.3
+
+    pkg_lib_run ./configure --prefix=/opt/package/gnupg-2.4.3 \
+        --docdir=/opt/package/gnupg-2.4.3/share/doc/gnupg-2.4.3
+
+    pkg_lib_run make -j$(nproc)
+
+    pkg_lib_run sudo make install
+}
+
+pkg_link() {
+    for dirname in bin libexec sbin share; do
+        pkg_lib_symlink_all /opt/package/gnupg-2.4.3/$dirname /opt/$dirname
+    done
+}
