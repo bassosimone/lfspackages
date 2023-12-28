843df7c2fdb50409ffe235112b3fcf592f455c23
Simone Basso <bassosimone@gmail.com>
2023-12-28 03:30:13 +0100

    feat: check with cached homebrew versions

---
+++ homebrew/bin/check.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import glob
+import json
+import os.path
+import sys
+
+
+def main():
+    for dirname in glob.glob("./ports/*/*"):
+
+        pkgname = dirname.split("/")[-1]
+
+        formula = f"./homebrew/formulae/{pkgname}.json"
+        if not os.path.exists(formula):
+            continue
+
+        with open(formula, "r") as filep:
+            version = json.load(filep)["versions"]["stable"]
+
+            package_bash_dir = f"{dirname}/{pkgname}-{version}"
+            if not os.path.isdir(package_bash_dir):
+                sys.stderr.write(f"warning: {package_bash_dir} does not exist\n")
+
+
+if __name__ == "__main__":
+    main()
